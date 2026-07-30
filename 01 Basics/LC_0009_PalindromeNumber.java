cf6689de73aa928ae2b3ab63aa65d006a0ec68e1
sks@1234567890 <naikshivamurthynaik@gmail.com>
2026-07-30 15:51:53 +0530

    Solved LC 9 - Palindrome Number

---
+++ 01 Basics/LC_0009_PalindromeNumber.java
@@ -0,0 +1,20 @@
+
+class Solution {
+    public boolean isPalindrome(int x) {
+
+        if (x < 0) {
+            return false;
+        }
+
+        int dup = x;
+        int reverse = 0;
+
+        while (x != 0) {
+            int lastDigit = x % 10;
+            reverse = reverse * 10 + lastDigit;
+            x = x / 10;
+        }
+
+        return reverse == dup;
+    }
+}
